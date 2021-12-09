ad17e393a90b4c0f7743eb80b3f4445f75a81614
ilhamtrz <ilhamtriza@gmail.com>
2021-12-09 13:31:11 +0700

    add route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const serverConfig = {
     port:5000,
@@ -7,7 +8,9 @@ const serverConfig = {
  
 const init = async () => {
     const server = Hapi.server(serverConfig);
- 
+
+    server.route(routes);
+    
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
