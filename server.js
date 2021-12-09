e11533b751bf1bf32c323b6e13f7db9048d8ad28
ilhamtrz <ilhamtriza@gmail.com>
2021-12-09 09:30:11 +0700

    setting default config

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
+const Hapi = require('@hapi/hapi');
+
+const serverConfig = {
+    port:5000,
+    host: 'localhost',
+}
+ 
+const init = async () => {
+    const server = Hapi.server(serverConfig);
+ 
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
